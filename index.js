0052f8a05a2cd780b5e46aeda2d96ea76c0776b5
Crina Bucur <bucur.crina@gmail.com>
2022-01-09 20:23:26 +0000

    Changed behavior to PR title checker

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-core.setFailed("Forced failure");
+try {
+  // Inputs defined in action metadata file
+  let jiraProjectKey = core.getInput('jira-project-key');
+  let noTicketPrefix = core.getInput('no-ticket-prefix');
+  const ignoreCase = core.getInput('ignore-case');
+  //const verbose = core.getInput('verbose');
 
-try {  
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  
+  let pullRequestTitle = getPullRequestTitle();
+  
+  let caseInsensitiveMode = (ignoreCase === 'true');
+  console.log(`Ignore case set to ${caseInsensitiveMode}`);
+  
+  if (caseInsensitiveMode) {
+    pullRequestTitle = pullRequestTitle.toUpperCase();
+    jiraProjectKey = jiraProjectKey.toUpperCase();
+  }
+  
+  let passes = false;
+
+  if (noTicketPrefix != null) {
+    if (caseInsensitiveMode) {
+      noTicketPrefix = noTicketPrefix.toUpperCase();
+    }
+
+    if (pullRequestTitle.startsWith('[' + noTicketPrefix + ']') === true) {
+      passes = true;
+    }
+  }
+  
+  if (passes === false){
+    let pattern = "^\\[" + jiraProjectKey + "-[0-9]+\\]\\ [^\\ ]";
+    let regExp = caseInsensitiveMode ? new RegExp(concatPattern,"ig") : new RegExp(concatPattern,"g");
+
+    passes = regExp.test(commitName);
+    if (passes === false)
+      core.setFailed('Invalid pull request prefix!');
+  }
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+function getPullRequestTitle() {
+  let pullRequest = github.context.payload.pull_request;
+  core.debug(`Pull Request: ${JSON.stringify(github.context.payload.pull_request)}`);
+  if (pullRequest == undefined || pullRequest.title == undefined) {
+    throw new Error("This action should only be run with Pull Request events");
+  }
+  return pullRequest.title;
 }
